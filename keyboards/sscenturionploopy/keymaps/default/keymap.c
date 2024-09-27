a7758e8177f45416ffe0313fd31f9cba5054e90c
Ryan Johnston <135160579+Glitch860@users.noreply.github.com>
2024-09-27 15:28:34 -0400

    Update keymap.c

--- keyboards/sscenturionploopy/keymaps/default/keymap.c
+++ keyboards/sscenturionploopy/keymaps/default/keymap.c
@@ -49,13 +49,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,          KC_W, KC_E,    KC_R,          KC_T,                      KC_MS_BTN2,             KC_Y,          KC_U,          KC_I,     KC_O,   KC_P,
         LCTL_T(KC_A), KC_S, KC_D,    KC_F,          KC_G,                      KC_MS_BTN1,             KC_H,          KC_J,          KC_K,     KC_L,   RCTL_T(KC_SCLN),
         LSFT_T(KC_Z),  KC_X, KC_C,    KC_V,          KC_B,                      DRAG_SCROLL,            KC_N,          KC_M,          KC_COMMA, KC_DOT, RSFT_T(KC_SLSH),
-                             KC_LGUI, LT(3,KC_SPC),  LT(1,KC_BSPC),                                     LT(1,KC_SPC),  LT(2,KC_ENT),  KC_LALT                        
+                             KC_LGUI, LT(3,KC_DEL),  LT(1,KC_BSPC),                                     LT(1,KC_SPC),  LT(2,KC_ENT),  KC_LALT                        
     ),
     [_FN1] = LAYOUT(
         KC_TAB,   KC_HOME, KC_UP,   KC_END,   KC_PSCR,              _______,                  KC_BRIU,  KC_PGUP, KC_UP,   KC_PGDN,  KC_GRV,
-        KC_LCTL,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_INS,               _______,                  KC_BRID,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_RCTL,
-        KC_LSFT, _______,  KC_MUTE, KC_VOLD, KC_VOLU,               _______,                  _______,  _______,  _______,  _______, KC_RSFT,
-                          _______, _______, _______,                                          _______, _______,  _______                   
+        KC_LCTL,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_INS,               _______,                  KC_BRID,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_QUOT,
+        KC_LSFT, _______,  KC_MUTE, KC_VOLD, KC_VOLU,               _______,                  _______,  _______,  _______,  KC_RCTL, KC_RSFT,
+                          KC_APP, _______, _______,                                          _______, _______,  _______                   
     ),
     [_FN2] = LAYOUT(
         KC_ESC,  _______,   _______,  KC_LBRC,  KC_MINS,           _______,                   KC_EQL,  KC_RBRC, _______, _______, KC_DEL,
